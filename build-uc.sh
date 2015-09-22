440c642a5c36eb7576af1510ca83a3dee0b2ab1e
Craig Condit <ccondit@randomcoder.org>
2015-09-22 18:23:29 -0500

    Finished builder

--- build-uc.sh
+++ build-uc.sh
@@ -6,5 +6,7 @@ cd "$(pwd -P)"/uc
 
 set -x
 docker build -t ccondit/uc:builder --pull - < Dockerfile.builder
-docker run --rm ccondit/uc:builder tar cC /build/root . | xz -z9 > uc.tar.xz
-
+docker run --rm=true ccondit/uc:builder tar cC /build/root . | xz -z9 > uc.tar.xz
+docker build -t ccondit/uc .
+docker run --rm=true ccondit/uc sh -xec 'true'
+docker run --rm=true ccondit/uc ping -c 1 google.com
