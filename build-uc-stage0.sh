09c417343dc4f38417bfa864867239f7102ed5e7
Craig Condit <ccondit@randomcoder.org>
2015-09-25 07:20:58 -0500

    Updated script

--- build-uc-stage0.sh
+++ build-uc-stage0.sh
@@ -5,8 +5,10 @@ cd "$(dirname $0)"
 cd "$(pwd -P)"/uc-stage0-builder
 
 docker build -t insideo/uc-stage0-builder --pull .
-docker run --rm=true insideo/uc-stage0-builder tar cC /build/root . | xz -z9 > ../uc-stage0/uc-stage0.tar.xz
-docker run --rm=true insideo/uc-stage0-builder tar cC /build/deb .> ../packages/stage0-packages.tar
+docker run --rm=true -v "$(pwd)/../uc-stage0":/output:rw insideo/uc-stage0-builder \
+	'tar cC /build/root . | xz -z9 > /output/uc-stage0.tar.xz'
+docker run --rm=true -v "$(pwd)/../packages":/output:rw insideo/uc-stage0-builder \
+	'tar cC /build/deb .> /output/stage0-packages.tar'
 cd ../uc-stage0
 docker build -t insideo/uc-stage0 .
 docker run --rm=true insideo/uc-stage0 sh -xec 'true'
