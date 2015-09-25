752ecdac9911bf7bc5a1fdca589ef66c1da533a6
Craig Condit <ccondit@randomcoder.org>
2015-09-24 22:20:10 -0500

    Fix paths

--- uc-stage0-builder/tools-resources/gcc-pass2-make-limits-h.sh
+++ uc-stage0-builder/tools-resources/gcc-pass2-make-limits-h.sh
@@ -1,2 +1,2 @@
 cat gcc/limitx.h gcc/glimits.h gcc/limity.h > \
-  `dirname $($LFS_TGT-gcc -print-libgcc-file-name)`/include-fixed/limits.h
+  `dirname $($TARGET_PLATFORM-gcc -print-libgcc-file-name)`/include-fixed/limits.h
