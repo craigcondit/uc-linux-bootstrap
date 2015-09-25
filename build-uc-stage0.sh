3bc6d19cd8e8c6a642b0d2a4e1c4ca940c96cc75
Craig Condit <ccondit@randomcoder.org>
2015-09-25 08:50:22 -0500

    Use straight tar to pipe data from stage0 builder to stage0

--- build-uc-stage0.sh
+++ build-uc-stage0.sh
@@ -7,15 +7,15 @@ cd "$(pwd -P)"/uc-stage0-builder
 chmod g+rws ../uc-stage0 ../packages || /bin/true
 setfacl -m "default:group::rw" ../uc-stage0 ../packages || /bin/true
 
-docker build -t insideo/uc-stage0-builder --pull .
-docker run --rm=true -v "$(pwd)/../uc-stage0":/output:rw insideo/uc-stage0-builder \
-	bash -c "tar cC /build/root . | gzip > /output/uc-stage0.tar.gz"
+time docker build -t insideo/uc-stage0-builder --pull .
+time docker run --rm=true -v "$(pwd)/../uc-stage0":/output:rw insideo/uc-stage0-builder \
+	bash -c "tar cC /build/root . > /output/uc-stage0.tar"
 
-docker run --rm=true -v "$(pwd)/../packages":/output:rw insideo/uc-stage0-builder \
-	bash -c "tar cC /build/deb .> /output/stage0-packages.tar"
+time docker run --rm=true -v "$(pwd)/../packages":/output:rw insideo/uc-stage0-builder \
+	bash -c "tar cC /build/deb . > /output/stage0-packages.tar"
 
 cd ../uc-stage0
-docker build -t insideo/uc-stage0 .
+time docker build -t insideo/uc-stage0 .
 docker run --rm=true insideo/uc-stage0 sh -xec 'true'
 set +x
 echo "Build complete."
