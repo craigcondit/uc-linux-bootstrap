27473a61b91e589c8fe5f8fd5fba99323fe9b2d8
Craig Condit <ccondit@randomcoder.org>
2015-09-24 22:22:28 -0500

    Fix script

--- uc-stage0-builder/tools-resources/gcc-pass2-make-limits-h.sh
+++ uc-stage0-builder/tools-resources/gcc-pass2-make-limits-h.sh
@@ -1,2 +1,2 @@
-cat gcc/limitx.h gcc/glimits.h gcc/limity.h > \
+TARGET_PLATFORM=$(uname -m)-uc-linux-gnu cat gcc/limitx.h gcc/glimits.h gcc/limity.h > \
   `dirname $($TARGET_PLATFORM-gcc -print-libgcc-file-name)`/include-fixed/limits.h
