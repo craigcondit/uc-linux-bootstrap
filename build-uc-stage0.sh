72fcaf8411a88843604da7805a9ab02c510d6b98
Craig Condit <ccondit@randomcoder.org>
2015-09-25 07:39:40 -0500

    Rework stage0

--- build-uc-stage0.sh
+++ build-uc-stage0.sh
@@ -4,11 +4,16 @@ set -ex
 cd "$(dirname $0)"
 cd "$(pwd -P)"/uc-stage0-builder
 
+chmod g+rws ../uc-stage0 ../packages || /bin/true
+setfacl -m "default:group::rw" ../uc-stage0 ../packages || /bin/true
+
 docker build -t insideo/uc-stage0-builder --pull .
 docker run --rm=true -v "$(pwd)/../uc-stage0":/output:rw insideo/uc-stage0-builder \
-	'tar cC /build/root . | xz -z9 > /output/uc-stage0.tar.xz'
+	bash -c "tar cC /build/root . | gzip > /output/uc-stage0.tar.gz"
+
 docker run --rm=true -v "$(pwd)/../packages":/output:rw insideo/uc-stage0-builder \
-	'tar cC /build/deb .> /output/stage0-packages.tar'
+	bash -c "tar cC /build/deb .> /output/stage0-packages.tar"
+
 cd ../uc-stage0
 docker build -t insideo/uc-stage0 .
 docker run --rm=true insideo/uc-stage0 sh -xec 'true'
