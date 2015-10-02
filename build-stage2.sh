6f9417c5f513d2104bab3b379342e59548a091c6
Craig Condit <ccondit@randomcoder.org>
2015-10-02 18:52:00 -0500

    Build stage 3

--- build-stage2.sh
+++ build-stage2.sh
@@ -4,8 +4,8 @@ set -ex
 cd "$(dirname $0)"
 cd "$(pwd -P)"/stage2
 
-#chmod g+rws ../stage3 ../packages-stage2 || /bin/true
-#setfacl -m "default:group::rw" ../stage3 ../packages-stage2 || /bin/true
+chmod g+rws ../stage3 ../packages-stage2 || /bin/true
+setfacl -m "default:group::rw" ../stage3 ../packages-stage2 || /bin/true
 
 time docker build -t insideo/uc-stage2 --pull .
 
@@ -14,17 +14,18 @@ if [ "$1" == "fast" ]; then
   exit 0
 fi
 
-#time docker run --rm=true -v "$(pwd)/../stage3":/output:rw insideo/uc-stage2 \
-#        bash -c "tar cC /stage3 . > /output/stage3.tar"
-#time docker run --rm=true -v "$(pwd)/../stage3":/output:rw insideo/uc-stage2 \
-#        bash -c "tar cC /packages . > /output/stage3-packages.tar"
-#
-#cd ../stage3
-#rm -f stage3.tar.xz || /bin/true
-#xz -9 stage3.tar
-#
-#cd ../packages-stage2
-#find -type f -name "*.deb" -delete
-#tar xf ../stage3/stage3-packages.tar
+time docker run --rm=true -v "$(pwd)/../stage3":/output:rw insideo/uc-stage2 \
+        sh -c "tar cC /stage3 . > /output/stage3.tar"
+time docker run --rm=true -v "$(pwd)/../packages-stage2":/output:rw insideo/uc-stage2 \
+        sh -c "tar cC /packages . > /output/stage2-packages.tar"
+
+cd ../stage3
+rm -f stage3.tar.xz || /bin/true
+xz -9 stage3.tar
+
+cd ../packages-stage2
+find -type f -name "*.deb" -delete
+tar xf stage2-packages.tar
+rm -f stage2-packages.tar
 
 echo "stage2 build complete."
